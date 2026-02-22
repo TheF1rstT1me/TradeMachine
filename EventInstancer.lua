871cd30ae664a27334f245efe72c65ea4443d747
Stanislav <ivanivanovix163@gmail.com>
2026-02-22 22:32:49 +0400

    Create EventInstancer.lua

---
+++ EventInstancer.lua
@@ -0,0 +1,26 @@
+local RS = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+local mainFolder = Instance.new("Folder", RS)
+mainFolder.Name = "_CacheEvents"
+
+local instances = {
+	{
+		Type = "BindableEvent",
+		Name = "StartChange",
+		Parent = RS:FindFirstChild("_CacheEvents")
+	}
+}
+
+for _, Info in pairs(instances) do
+	task.spawn(function()
+		local obj = Instance.new(Info.Type, Info.Parent)
+		obj.Name = Info.Name
+	end)
+end
+
+while #mainFolder:GetChildren() ~= #instances do
+	task.wait()
+end
+
+-- loadstring
